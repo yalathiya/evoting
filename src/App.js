4b93cea8130b38332702baf3a725e7dadda62228
yalathiya <yashlathiya59ppsv@gmail.com>
2022-11-20 17:02:55 +0530

    Added mdb bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import VotingApp from "./VotingApp";
 import NavbarCard from "./components/NavbarCard.jsx";
+import LoginCard from "./components/LoginCard";
 const App = () => {
   document.title = `E-Voting`;
   return (
     <>
       <NavbarCard />
-      <VotingApp />
+      <LoginCard />
+      {/* <VotingApp /> */}
     </>
   );
 };

8903a901daa3a8571367e7e98e1c0136fe76f943
yalathiya <yashlathiya59ppsv@gmail.com>
2022-11-20 16:32:20 +0530

    added VotingApp.jsx file

--- src/App.js
+++ src/App.js
@@ -1,42 +1,12 @@
-import React, { useEffect, useState } from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import VotingCard from "./components/VotingCard";
-import teamsJson from "./lib/teams.json";
-import "./assets/scss/styles.scss";
-import "bootstrap/dist/css/bootstrap.css";
+import VotingApp from "./VotingApp";
 
-function App() {
-  let [teams, setTeams] = useState([]);
-
-  useEffect(() => {
-    setTeams(teamsJson);
-  }, []);
-
-  function incrementVoteCount(teamId) {
-    teams = teams.map((team) => {
-      if (team._id === teamId) {
-        team.votes = team.votes + 1;
-      }
-      return team;
-    });
-    setTeams(teams);
-  }
-
+const App = () => {
+  document.title = `E-Voting`;
   return (
-    <Container className="app">
-      <Row>
-        {teams.map((team) => {
-          return (
-            <Col md={4}>
-              <VotingCard
-                team={team}
-                incrementVoteCount={(teamId) => incrementVoteCount(teamId)}
-              />
-            </Col>
-          );
-        })}
-      </Row>
-    </Container>
+    <>
+      <VotingApp />
+    </>
   );
-}
+};
+
 export default App;
